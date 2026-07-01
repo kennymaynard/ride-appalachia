d6805911d8bde6299ee042e04c20607de804b117
k <kennymaynard_07@yahoo.com>
2026-07-01 18:03:17 -0400

    Add reliable frontend typecheck

---
+++ frontend/env.d.ts
@@ -0,0 +1,9 @@
+declare const process: {
+  env: {
+    API_URL?: string;
+    NEXT_PUBLIC_API_BASE_URL?: string;
+    NEXT_PUBLIC_API_URL?: string;
+    NODE_ENV?: string;
+    SKIP_API_DURING_BUILD?: string;
+  };
+};
